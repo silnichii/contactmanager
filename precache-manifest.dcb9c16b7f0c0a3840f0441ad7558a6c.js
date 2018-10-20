0e458ec473add2c3fe31a8f0a62b53b7cf5dd12a
silnichii <silnichii@gmail.com>
2018-10-20 14:46:23 +0200

    Updates

---
+++ precache-manifest.dcb9c16b7f0c0a3840f0441ad7558a6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "241f14c063544b821405",
+    "url": "/contactmanager/static/js/main.241f14c0.chunk.js"
+  },
+  {
+    "revision": "86c6a71ba02f877d7ad3",
+    "url": "/contactmanager/static/js/1.86c6a71b.chunk.js"
+  },
+  {
+    "revision": "241f14c063544b821405",
+    "url": "/contactmanager/static/css/main.58dd30b2.chunk.css"
+  },
+  {
+    "revision": "86c6a71ba02f877d7ad3",
+    "url": "/contactmanager/static/css/1.524b6c7a.chunk.css"
+  },
+  {
+    "revision": "95a42acf45478f51d197a71d84bc169b",
+    "url": "/contactmanager/index.html"
+  }
+];
